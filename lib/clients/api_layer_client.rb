43c3e74eb3d91f9d8e078b66a6b6f0ce3632886b
georgetim1998 <georgetim1998@gmail.com>
2022-09-27 19:38:23 +0300

    servise done

--- lib/clients/api_layer_client.rb
+++ lib/clients/api_layer_client.rb
@@ -3,4 +3,30 @@ require 'net/http'
 
 class ApiLayerClient
   ROOT_ENDPOINT = 'https://api.apilayer.com/currency_data'.freeze
+
+  def list
+    url = URI("#{ROOT_ENDPOINT}/list")
+
+    send_request(url)
+  end
+
+  def live(currensies, source)
+    url = URI("#{ROOT_ENDPOINT}/live?source=#{source}&currencies=#{currensies}")
+
+    send_request(url)
+  end
+
+  private
+
+  def send_request(url)
+    https = Net::HTTP.new(url.host, url.port)
+    https.use_ssl = true
+
+    request = Net::HTTP::Get.new(url)
+    request['apikey'] = 'RY6ogV9vWxpI5xh42I6x177IC2NYrFi7'
+
+    response = https.request(request).read_body
+
+    JSON.parse(response)
+  end
 end
