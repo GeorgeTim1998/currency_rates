6225a6b17cf312aac96190c1f9081456e6a963f7
georgetim1998 <georgetim1998@gmail.com>
2022-09-29 15:42:49 +0300

    simple front done

--- app/models/currency.rb
+++ app/models/currency.rb
@@ -4,6 +4,22 @@ class Currency < ApplicationRecord
   attr_accessor :buffer
 
   def readable_time
-    Time.at(self.buffer['timestamp']).to_datetime.to_s
+    Time.at(buffer['timestamp']).to_datetime.to_s
+  end
+
+  def currencies_name
+    buffer["currencies"].keys
+  end
+
+  def quotes
+    buffer['quotes']
+  end
+  
+  def ratio_currencies
+    quotes.keys[0]
+  end
+  
+  def ratio
+    quotes[ratio_currencies]
   end
 end
