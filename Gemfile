9ad695ce921ac55423de26605c6c1a8bf56e3a17
georgetim1998 <georgetim1998@gmail.com>
2022-10-04 21:37:04 +0300

    elastic fail!

--- Gemfile
+++ Gemfile
@@ -25,7 +25,11 @@ gem 'jbuilder', '~> 2.7'
 # Use Active Storage variant
 # gem 'image_processing', '~> 1.2'
 
-gem 'faraday'
+# gem 'faraday'
+
+gem 'elasticsearch-model'
+gem 'elasticsearch-persistence'
+gem 'elasticsearch-rails'
 
 # Reduces boot times through caching; required in config/boot.rb
 gem 'bootsnap', '>= 1.4.4', require: false
--- app/models/currency.rb
+++ app/models/currency.rb
@@ -1,6 +1,10 @@
 require 'date'
+require 'elasticsearch/model'
 
 class Currency < ApplicationRecord
+  include Elasticsearch::Model
+  include Elasticsearch::Model::Callbacks
+
   attr_accessor :buffer
 
   def readable_time
@@ -8,17 +12,17 @@ class Currency < ApplicationRecord
   end
 
   def currencies_name
-    buffer["currencies"].keys
+    buffer['currencies'].keys
   end
 
   def quotes
     buffer['quotes']
   end
-  
+
   def ratio_currencies
     quotes.keys[0]
   end
-  
+
   def ratio
     quotes[ratio_currencies]
   end
